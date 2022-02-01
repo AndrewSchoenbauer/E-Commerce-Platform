91434ce702f0a9b50e3a83d7b23283e681a34703
Andrew <andrewschoenbauer12@gmail.com>
2022-02-01 16:50:49 -0600

    finished and tested category routes and all are functioning

--- models/index.js
+++ models/index.js
@@ -13,19 +13,19 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
-})
-// Tags belongToMany Products (through ProductTag)
-Tag.belongstoMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  }
-})
+// Product.belongsToMany(Tag, {
+//   through: {
+//     model: ProductTag,
+//     unique: false
+//   }
+// })
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongstoMany(Product, {
+//   through: {
+//     model: ProductTag,
+//     unique: false
+//   }
+// })
 module.exports = {
   Product,
   Category,
